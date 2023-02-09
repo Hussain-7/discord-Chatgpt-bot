bae3b0cc0400f5f239395b0d505d926c84b95f37
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-02-09 23:57:23 +0500

    Message reation adde

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+console.log("GatewayIntentBits: ", GatewayIntentBits);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
 
 require("dotenv").config();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async (message) => {
+  if (message.content === "ping") {
+    message.channel.send("Pong!");
+  }
+});
+
+client.on("messageCreate", async (message) => {
+  console.log("Message registered!");
 });
 
 client.login(process.env.BOT_TOKEN);
