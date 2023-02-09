03eaea922a81df708d7ca7212c778ef010423f06
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-02-09 23:30:59 +0500

    bot connected now

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();	
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+require("dotenv").config();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login(process.env.BOT_TOKEN);
