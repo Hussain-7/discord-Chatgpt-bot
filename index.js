3024692dc6b5cc1dae208572c284de80f3b199b0
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2023-02-10 01:13:21 +0500

    Integrate Chatgpt

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const client = new Client({
   ],
 });
 
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
 require("dotenv").config();
 
 client.on("ready", () => {
@@ -25,8 +30,33 @@ client.on("message", async (message) => {
 client.on("messageCreate", async (message) => {
   console.log("Message registered!", message.content);
   const { content } = message;
-  if (content === "ping") {
-    message.reply("Pong!");
+
+  if (message.author.bot) return;
+
+  switch (content) {
+    case "ping":
+      message.reply("Pong!");
+      break;
+    case "what is my avatar":
+      message.reply(message.author.displayAvatarURL());
+      break;
+    case "who are you":
+      message.reply("I am a bot created by Hussain Rizvi");
+      break;
+    case "hi":
+      message.reply("Hello!");
+      break;
+    default:
+      // reply from chat gpt-3
+      const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: content,
+        max_tokens: 7,
+        temperature: 0,
+      });
+      console.log(response);
+      message.reply(response.data.choices[0].text);
+      break;
   }
 });
 
